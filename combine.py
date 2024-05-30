f993f9678ad92119abd060c90624b7cd92d1ec9f
jklim24 <ljk00221@gmail.com>
2024-05-30 23:27:29 +0900

    Update combine.py

--- combine.py
+++ combine.py
@@ -3,5 +3,7 @@ def pec(i):
         return 1
     else:
         return pec(i-1)*i
-n,k=map(int,input().split())
-print(int(pec(n)/pec(k)/pec(n-k)))
+t=int(input())
+for i in range(0,t):
+    k,n=map(int,input().split())
+    print(int(pec(n)//pec(k)//pec(n-k)))
